060fb3569a99d2f5b08053413bdb556db19898f1
onteriowright <onteriowright@yahoo.com>
2020-03-10 11:29:00 -0500

    added more instances

--- Program.cs
+++ Program.cs
@@ -6,67 +6,67 @@ namespace FlowerShop
     {
         static void Main(string[] args)
         {
-            // Rose Rose = new Rose()
-            // {
-            //     Name = "Rosey",
-            //     StemLength = 5,
-            //     Type = "Rose Bush",
-            //     Cost = 10.00,
-            //     Water_Schedule = "2x a week",
-            //     Thorny = true,
-            //     Fragrant = "Fresh"
-            // };
+            Rose Rose = new Rose()
+            {
+                Name = "Rosey",
+                StemLength = 5,
+                Type = "Rose Bush",
+                Cost = 10.00,
+                Water_Schedule = "2x a week",
+                Thorny = true,
+                Fragrant = "Fresh"
+            };
 
-            // Irises Irises = new Irises()
-            // {
-            //     Color = "Blue",
-            //     Name = "Irie",
-            //     Type = "Flower",
-            //     Cost = 5.00,
-            //     Water_Schedule = "1x a week",
-            //     Thorny = false,
-            //     Fragrant = "Minty"
-            // };
+            Irises Irises = new Irises()
+            {
+                Color = "Blue",
+                Name = "Irie",
+                Type = "Flower",
+                Cost = 5.00,
+                Water_Schedule = "1x a week",
+                Thorny = false,
+                Fragrant = "Minty"
+            };
 
-            // Sunflower Sunflower = new Sunflower()
-            // {
-            //     Color = "Yellow",
-            //     Name = "Sunny",
-            //     Type = "Flower",
-            //     Cost = 3.00,
-            //     Water_Schedule = "1x a week",
-            //     Thorny = false,
-            //     Fragrant = "Fresh"
-            // };
+            SunFlower Sunflower = new SunFlower()
+            {
+                Color = "Yellow",
+                Name = "Sunny",
+                Type = "Flower",
+                Cost = 3.00,
+                Water_Schedule = "1x a week",
+                Thorny = false,
+                Fragrant = "Fresh"
+            };
 
-            // Tulip Tulip = new Tulip()
-            // {
-            //     Name = "Tully",
-            //     StemLength = 5,
-            //     Type = "Flower",
-            //     Cost = 3.00,
-            //     Water_Schedule = "1x a week",
-            //     Thorny = false,
-            //     Fragrant = "Fresh"
-            // };
+            Tulip Tulip = new Tulip()
+            {
+                Name = "Tully",
+                StemLength = 5,
+                Type = "Flower",
+                Cost = 3.00,
+                Water_Schedule = "1x a week",
+                Thorny = false,
+                Fragrant = "Fresh"
+            };
 
-            // SnapDragon snapDragon = new SnapDragon()
-            // {
-            //     Name = "Tully",
-            //     StemLength = 5,
-            //     Color = "Green",
-            //     Type = "Flower",
-            //     Cost = 3.00,
-            //     Water_Schedule = "1x a week",
-            //     Thorny = false,
-            //     Fragrant = "Fresh"
-            // };
+            SnapDragon snapDragon = new SnapDragon()
+            {
+                Name = "Tully",
+                StemLength = 5,
+                Color = "Green",
+                Type = "Flower",
+                Cost = 3.00,
+                Water_Schedule = "1x a week",
+                Thorny = false,
+                Fragrant = "Fresh"
+            };
 
             Rose redRose = new Rose();
             SunFlower sunFlower = new SunFlower();
             Irises irises = new Irises();
             Tulip tulip = new Tulip();
-            SnapDragon snapDragon = new SnapDragon();
+            SnapDragon SnapDragon = new SnapDragon();
         }
     }
 }
